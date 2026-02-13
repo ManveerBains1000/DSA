d7d13d48d0271e797424d4ebff451b6f67884ba0
ManveerBains1000 <manveerjaat95@gmail.com>
2026-02-13 21:45:36 +0530

    Add Main class to implement partition labels algorithm

---
+++ Greedy/partition-labels/Main.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Main {
+    public List<Integer> partitionLabels(String s) {
+        int n = s.length();
+        int[] last = new int[26];
+        for (int i = 0; i < n; i++) {
+            last[s.charAt(i)-'a'] = i;
+        }  
+        int start = 0,end = 0;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            end = Math.max(end,last[s.charAt(i)-'a']);
+            if (i == end) {
+                ans.add(end-start+1);
+                start = i+1;
+            }
+        }
+        return ans;
+    }
+}
