ade492b2792695c180ff6ce942941511574bdd21
ManveerBains1000 <manveerjaat95@gmail.com>
2026-03-05 22:33:52 +0530

    Add Main class to implement Minimum Changes to Make Alternating Binary String algorithm

---
+++ Strings/Minimum-Changes-To-Make-Alternating-Binary-String/Main.java
@@ -0,0 +1,23 @@
+class Main {
+    public int minOperations(String s) {
+        int count1 = 0; // 0101
+        int count2 = 0; // 1010
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (i % 2 == 0) {
+                if (ch != '0') count1++;
+            }
+            else {
+                if (ch != '1') count1++;
+            }
+            if (i % 2 == 0) {
+                if (ch != '1') count2++;
+            }
+            else {
+                if (ch != '0') count2++;
+            }
+        }
+        return Math.min(count1,count2);
+    }
+}
